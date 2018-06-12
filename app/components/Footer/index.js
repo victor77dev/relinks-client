d26bc80b5cbf36b9a4bc84ca40e2d1741001ec5c
victor77dev <victor77.dev@gmail.com>
2018-06-12 09:23:33 +0200

    Copy Helmet, Header and Footer from example

---
+++ app/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import Wrapper from './Wrapper';
+import messages from './messages';
+
+function Footer() {
+  return (
+    <Wrapper>
+      <section>
+        <FormattedMessage
+          {...messages.authorMessage}
+          values={{
+            author: <p>Victor</p>,
+          }}
+        />
+      </section>
+    </Wrapper>
+  );
+}
+
+export default Footer;
