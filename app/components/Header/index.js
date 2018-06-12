aeb11b382d99db165032bc9207487416ff872770
victor77dev <victor77.dev@gmail.com>
2018-06-12 15:29:48 +0200

    Create material ui theme

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,6 +9,27 @@ import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import grey from '@material-ui/core/colors/grey';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+// Creating theme with following colors
+// https://material.io/tools/color/#!/?view.left=0&view.right=0&secondary.color=455A64&primary.color=212121
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#484848',
+      main: grey[900],
+      dark: '#000000',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#718792',
+      main: blueGrey[700],
+      dark: '#1c313a',
+      contrastText: '#ffffff',
+    },
+  },
+});
 
 const styles = {
   menuButton: {
@@ -55,24 +76,26 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
     const { classes } = this.props;
     const buttons = ['Home', 'Testing'];
     return (
-      <AppBar>
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon onClick={this.handleMenuClick} />
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.handleMenuClose}
-              className={classes.menu}
-            >
-              {this.addButtons(buttons)}
-            </Menu>
-          </IconButton>
-          <Typography variant="title" color="inherit">
-            ReLinks
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <MuiThemeProvider theme={theme}>
+        <AppBar>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon onClick={this.handleMenuClick} />
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleMenuClose}
+                className={classes.menu}
+              >
+                {this.addButtons(buttons)}
+              </Menu>
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              ReLinks
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </MuiThemeProvider>
     );
   }
 }
