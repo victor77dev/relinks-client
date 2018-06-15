6182760b0163b15671bbb588c4612a49da949f43
victor77dev <victor77.dev@gmail.com>
2018-06-15 18:41:25 +0200

    Display Paper Data in Search Result

---
+++ app/components/Paper/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Paper from '../index';
+
+describe('<Paper />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
--- app/components/SearchResult/index.js
+++ app/components/SearchResult/index.js
@@ -8,18 +8,28 @@ import React from 'react';
 // import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import Paper from 'components/Paper';
+
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+/* eslint-disable no-underscore-dangle */
 class SearchResult extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     const { searchResult } = this.props;
+    let paperList;
+    if (!searchResult || searchResult.length === 0) {
+      paperList = <h3>Cannot find related paper in ReLinks</h3>;
+    } else {
+      paperList = searchResult.map((data) => <Paper paperData={data} key={data._id} />);
+    }
+
     return (
       <div>
         <h1>
           <FormattedMessage {...messages.header} />
         </h1>
-        {JSON.stringify(searchResult)}
+        {paperList}
       </div>
     );
   }
