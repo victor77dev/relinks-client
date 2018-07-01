9cb6f5ab8540fd34fc4d7d0e375c3f7ebec3f2dc
victor77dev <victor77.dev@gmail.com>
2018-07-01 21:41:26 +0200

    Add api call for update paper info in CardLayoutContainer saga

--- app/containers/CardLayoutContainer/saga.js
+++ app/containers/CardLayoutContainer/saga.js
@@ -1,6 +1,29 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import axios from 'axios';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { UPDATE_PAPER_INFO } from 'containers/CardLayoutContainer/constants';
+import { updateLoaded, updateError, stopEdit } from 'containers/CardLayoutContainer/actions';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+// const config = require('../config.json')
+const apiUrl = 'http://localhost:4000';
+
+export function* updatePaperInfo(action) {
+  const { paperId, paperInfo } = action;
+  try {
+    // Call updatePaper api
+    const update = yield call(axios.get, `${apiUrl}/updatePaper`, {
+      params: {
+        paperId,
+        paperInfo: JSON.stringify(paperInfo),
+      },
+    });
+    // Call updateLoaded
+    yield put(updateLoaded(update.data));
+    yield put(stopEdit());
+  } catch (err) {
+    yield put(updateError(err));
+  }
+}
+
+export default function* cardLayoutSaga() {
+  yield takeLatest(UPDATE_PAPER_INFO, updatePaperInfo);
 }
